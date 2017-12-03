a91207a556202e919410d43c90483c74f41bc01b
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-03 20:42:31 +0200

    NewsDatabaseApplication: reformat code

--- src/main/java/news/NewsDatabaseApplication.java
+++ src/main/java/news/NewsDatabaseApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NewsDatabaseApplication {
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
         SpringApplication.run(NewsDatabaseApplication.class, args);
     }
-    
+
 }
