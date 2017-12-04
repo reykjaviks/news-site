a8fb14b301fe58c5f885ed63d5b9f779b9378ccc
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-04 22:04:22 +0200

    NewsRepository: rename class to ArticleRepository

--- src/main/java/news/repository/NewsRepository.java
+++
@@ -1,7 +0,0 @@
-package news.repository;
-
-import news.domain.News;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NewsRepository extends JpaRepository<News, Long> {
-}
