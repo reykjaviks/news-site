94fb5e895f1cb2ba334702eca3074072fdf8a849
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-06 21:44:52 +0200

    NewsController: use ModelAttribute and sort results

--- src/main/java/news/controller/NewsController.java
+++ src/main/java/news/controller/NewsController.java
@@ -1,33 +1,40 @@
 package news.controller;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import news.domain.Article;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import news.repository.ArticleRepository;
+import news.repository.WriterRepository;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class NewsController {
-    
+
     @Autowired
     private ArticleRepository articleRepository;
-    
+    @Autowired
+    private WriterRepository writerRepository;
+
     @PostMapping("/")
-    public String addArticle(@RequestParam String title, @RequestParam String caption) {
-        articleRepository.save(new Article(title, caption, LocalDate.now()));
+    public String addArticle(@ModelAttribute Article article) {
+        article.setPubDate(LocalDateTime.now());
+        articleRepository.save(article);
         return "redirect:/";
     }
-    
+
     @RequestMapping("/")
     public String listNews(Model model) {
-        model.addAttribute("news", articleRepository.findAll());
-       return "index"; 
+        Pageable pageable = PageRequest.of(0, 5, Sort.Direction.DESC, "pubDate");
+        model.addAttribute("news", articleRepository.findAll(pageable));
+        return "index";
     }
     
     @RequestMapping("/{id}")
--- src/main/java/news/domain/Article.java
+++ src/main/java/news/domain/Article.java
@@ -1,11 +1,9 @@
 package news.domain;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,7 +18,7 @@ public class Article extends AbstractPersistable<Long> {
     
     private String title;
     private String caption;
-    private LocalDate pubDate;
+    private LocalDateTime pubDate;
     @OneToMany
     private List<Writer> writers;
     
