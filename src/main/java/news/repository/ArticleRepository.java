cf683c11e6936c05c5c86d76a8022a66873be9aa
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-08 11:54:54 +0200

    ArticleRepository: add repository annotation

--- src/main/java/news/repository/ArticleRepository.java
+++ src/main/java/news/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package news.repository;
 
 import news.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
