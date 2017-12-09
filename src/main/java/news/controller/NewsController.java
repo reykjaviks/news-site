c5bcd6630ed4c76079247cbc1ce3a66a0d575004
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-09 20:24:30 +0200

    Link a category to a list of articles

--- src/main/java/news/controller/NewsController.java
+++ src/main/java/news/controller/NewsController.java
@@ -3,6 +3,7 @@ package news.controller;
 import java.time.LocalDateTime;
 import javax.annotation.PostConstruct;
 import news.domain.Article;
+import news.domain.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import news.repository.ArticleRepository;
 import news.repository.CategoryRepository;
-import news.repository.WriterRepository;
 import news.service.ArticleConfigService;
 import news.service.CategoryConfigService;
 import org.springframework.data.domain.PageRequest;
@@ -25,20 +25,27 @@ public class NewsController {
     @Autowired
     private ArticleRepository articleRepository;
     @Autowired
+    private CategoryRepository categoryRepository;
+    @Autowired
     private ArticleConfigService articleConfigService;
     @Autowired
     private CategoryConfigService categoryConfigService;
 
-
     @PostConstruct
-    private String addNewsBase() {
-        articleRepository.save(articleConfigService.abduction());
-        articleRepository.save(articleConfigService.driverlessBuses());
+    public String init() {
+        categoryConfigService.initialize();
+        categoryRepository.saveAll(categoryConfigService.getAllCategories());
+
+        articleConfigService.initialize();
+        articleRepository.saveAll(articleConfigService.getAllArticles());
+
         return "index";
     }
 
     @PostMapping("/")
     public String addArticle(@ModelAttribute Article article) {
+        Category category = categoryRepository.findByName(article.getCategory());
+        category.getArticles().add(article); //Nyt mun kategoriaan on liitetty uutinen
         article.setPubDate(LocalDateTime.now());
         articleRepository.save(article);
         return "redirect:/";
@@ -47,15 +54,24 @@ public class NewsController {
     @RequestMapping("/")
     public String listNews(Model model) {
         Pageable pageable = PageRequest.of(0, 5, Sort.Direction.DESC, "pubDate");
-        model.addAttribute("categories", categoryConfigService.getAllCategories());
+        model.addAttribute("categories", categoryRepository.findAll());
         model.addAttribute("news", articleRepository.findAll(pageable));
         return "index";
     }
-    
+
     @RequestMapping("/{id}")
     public String retrieveArticle(Model model, @PathVariable Long id) {
+        model.addAttribute("category", articleRepository.getOne(id).getCategory());
         model.addAttribute("article", articleRepository.getOne(id));
         return "articleItem";
+    }
+
+    @RequestMapping("/categories/{name}")
+    public String listArticlesByCategory(Model model, @PathVariable String name) {
+        Category category = categoryRepository.findByName(name);
+        model.addAttribute("category", category);
+        model.addAttribute("news", category.getArticles());
+        return "category";
     }
 
 }
--- src/main/java/news/domain/Category.java
+++ src/main/java/news/domain/Category.java
@@ -1,17 +1,38 @@
 package news.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 @Entity
 public class Category extends AbstractPersistable<Long> {
     
     private String name;
+    @OneToMany
+    private List<Article> articles;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Article> getArticles() {
+        if (this.articles == null) {
+            this.articles = new ArrayList<>();
+        }
+        return this.articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
 
 }
