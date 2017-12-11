416b15aa59cac33d1076017de205537cfae3847f
Elizabeth Berg <elizabethinposti@gmail.com>
2017-12-11 22:50:10 +0200

    Add test class skeletons

---
+++ src/test/java/news/service/ArticleConfigServiceTest.java
@@ -0,0 +1,11 @@
+package news.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ArticleConfigServiceTest {
+    // TODO: supply tests
+}
